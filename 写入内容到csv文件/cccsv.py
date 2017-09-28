f2dc9403c23bedd2458e022b6fb9ca67c9673766
invincible1900 <395984722@qq.com>
2017-09-28 09:34:36 +0800

    Update cccsv.py

--- 写入内容到csv文件/cccsv.py
+++ 写入内容到csv文件/cccsv.py
@@ -5,13 +5,7 @@ sys.setdefaultencoding('utf-8')
 import csv
 csvfile = file('TmpUserList.csv', 'wb')
 writer = csv.writer(csvfile)
-# writer.writerow(['id', 'pwd', '有效期','分组','备注'])
 
-# data = [
-#   ('1', 'http://www.xiaoheiseo.com/', '小黑'),
-#   ('2', 'http://www.baidu.com/', '百度'),
-#   ('3', 'http://www.jd.com/', '京东')
-# ]
 data = []
 for i in range(1000):
 	data.append(('user'+str(i),'1','12h','tmp','Test'))
